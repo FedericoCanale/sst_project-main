734051d0c3759ceff429c918ce9b5144d9f5293e
FedericoCanale <icocanale@gmail.com>
2025-12-17 15:20:37 +0100

    installed all programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import viaggi from "./data/data"
+
 function App() {
 
   return (
     <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+      {console.log(viaggi)}
+
+
     </>
   )
 }
